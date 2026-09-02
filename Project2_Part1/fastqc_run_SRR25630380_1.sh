838b3921dff5793127da57e228c36fd1c5155c70
ZachBurge <zburge200@gmail.com>
2026-09-02 15:08:11 -0700

    Moving bash scripts for fastqc runs into Project2_Part1 folder

---
+++ Project2_Part1/fastqc_run_SRR25630380_1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --cpus-per-task=8
+#SBATCH --time=1-00:00
+
+/usr/bin/time -v pixi run fastqc SRR25630380_1.fastq -o /projects/bgmp/zburge/bioinfo/Bi623/Project2/Project2_QAA/SRR25630380_metrics
