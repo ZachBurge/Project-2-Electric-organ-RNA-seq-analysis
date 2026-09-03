791365a0bc3d64cb0c63e65fac7a626f112d84cc
ZachBurge <zburge200@gmail.com>
2026-09-03 12:13:21 -0700

    Rerunning htseq-counts with Parent instead of ID, and split the bash script into individual runs

---
+++ Project2_Part3/htseq-count_380_rev.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --account=bgmp
+#SBATCH --partition=bgmp
+#SBATCH --time=1-00:00:00
+#SBATCH --cpus-per-task=8
+
+/usr/bin/time -v pixi run htseq-count -s reverse -i Parent -n 8 \
+    /projects/bgmp/zburge/bioinfo/Bi623/Project2/Project2_QAA/Project-2-Electric-organ-RNA-seq-analysis/Project2_Part3/star_align_results/SRR25630380/Aligned.out.sam \
+    /projects/bgmp/zburge/bioinfo/Bi623/Project2/Project2_QAA/Project-2-Electric-organ-RNA-seq-analysis/Project2_Part3/campylomormyrus.gff > SRR25630380_counts_stranded_reverse
